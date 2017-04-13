73c1b455d7379aee2705fee531dfa4218090b036
Ethan Garofolo <ethan@suchsoftware.com>
2017-04-13 00:55:08 -0600

    Put back to correct dataset

--- anchor_words/experiments/run_sotu.py
+++ anchor_words/experiments/run_sotu.py
@@ -4,6 +4,6 @@ from read_dataset import (tokenize_dataset)
 
 print('Process sotu')
 random = numpy.random.RandomState(1)
-raw_data = tokenize_dataset('test')
+raw_data = tokenize_dataset('sotu')
 
 process_dataset(raw_data, random)
