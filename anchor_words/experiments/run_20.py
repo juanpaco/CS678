dcaecf171dfe138199cbdb0b92d30a3f00b83623
Ethan Garofolo <ethan@suchsoftware.com>
2017-04-19 07:25:16 -0600

    Use correct args

--- anchor_words/experiments/run_20.py
+++ anchor_words/experiments/run_20.py
@@ -4,4 +4,4 @@ from anchor_words import (process_dataset)
 print('Process 20')
 random = numpy.random.RandomState(1)
 
-process_dataset(raw_data, random, inclusion_threshold=2)
+process_dataset('20', random, inclusion_threshold=2)
--- anchor_words/experiments/run_clinton.py
+++ anchor_words/experiments/run_clinton.py
@@ -4,4 +4,4 @@ from anchor_words import (process_dataset)
 print('Process clinton')
 random = numpy.random.RandomState(1)
 
-process_dataset(raw_data, random, inclusion_threshold=2)
+process_dataset('clinton', random, inclusion_threshold=2)
